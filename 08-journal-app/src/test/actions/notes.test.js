f33c1a916e38f60bc2538560b26a90d4478b3a82
Javier Lucero <javimcl1986@gmail.com>
2022-02-21 20:29:00 -0500

    continuacion

--- 08-journal-app/src/test/actions/notes.test.js
+++ 08-journal-app/src/test/actions/notes.test.js
@@ -1,17 +1,21 @@
 import configureStore from 'redux-mock-store' 
 import thunk from 'redux-thunk';
+import { startNewNote } from '../../actions/notes';
 
  
 const middlewares = []
 const mockStore = configureStore(middlewares);
 
 const store = mockStore({
+    auth: {
+        uid: 'TESTING'
+    }
 
 })
 
 describe('Pruebas con las acciones de notes', () => { 
 
-    test('debe de crear una nueva nota startNewNote', () => { 
-
+    test('debe de crear una nueva nota startNewNote', async() => { 
+       await store.dispatch(startNewNote());
      })
  })
