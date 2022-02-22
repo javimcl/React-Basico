1ae88ea43675c925724ae0eda5d8cc07c980d365
Javier Lucero <javimcl1986@gmail.com>
2022-02-21 22:26:47 -0500

    c

--- 08-journal-app/src/firebase/firebase-config.js
+++ 08-journal-app/src/firebase/firebase-config.js
@@ -12,6 +12,22 @@ const firebaseConfig = {
     appId: "1:35185892252:web:80bced4d65fab29a430659"
   };
 
+  const firebaseConfigTesting = {
+    apiKey: "AIzaSyDvOVugw7KL67ix5WjpcuS6ZPj5uLnAmLg",
+    authDomain: "sistema-f6ba5.firebaseapp.com",
+    projectId: "sistema-f6ba5",
+    storageBucket: "sistema-f6ba5.appspot.com",
+    messagingSenderId: "591018828858",
+    appId: "1:591018828858:web:a303eb9b18ff6e5c3b8103",
+    measurementId: "G-R4DQCL8T59"
+  };
+
+  if (process.env.NODE_ENV === 'test') {
+      
+  } else {
+
+  }
+
   firebase.initializeApp(firebaseConfig);
 
   const db = firebase.firestore();
